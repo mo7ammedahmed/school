97a0ecea8496fd796d994270301ad12d8b7a0914
mo7ammedahmed <mo7ammed99887@gmail.com>
2026-09-10 00:06:07 +0300

    refactor: remove unused backup files and scripts

--- check.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new PDO('sqlite:D:/project/school/database/database.sqlite');
-$stmt = $db->query('SELECT name FROM sqlite_master WHERE type="table"');
-$tables = $stmt->fetchAll(PDO::FETCH_COLUMN);
-print_r($tables);
